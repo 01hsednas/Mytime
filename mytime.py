c586af72b1f3033cbc69ab0e3d37daeecf05e598
Sandesh Lohani <sandeshlohani560@gmail.com>
2024-01-09 09:06:03 +0545

    my time

---
+++ mytime.py
@@ -0,0 +1,14 @@
+import time
+name=input("Enter your name: ").capitalize()
+timestamp=time.strftime('%H:%M:%S')
+print(timestamp)
+hour=int(time.strftime('%H'))
+print(hour)
+if hour<=6 or hour<12:
+    print("Good morning",name)
+elif hour<=12 or hour<18:
+    print("Good afternoon",name)
+elif hour<=18 or hour<21:
+    print("Good evening",name)
+else:
+    print("Good night",name)
