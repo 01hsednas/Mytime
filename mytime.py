703f230cb28b8f9e8b3622397459463fd46c41b8
Saksham <perfectdude429@gmail.com>
2024-01-09 09:21:02 +0545

    removing print hour

--- mytime.py
+++ mytime.py
@@ -1,9 +1,13 @@
 import time
 name=input("Enter your name: ").capitalize()
 timestamp=time.strftime('%H:%M:%S')
+
 print(timestamp)
+
 hour=int(time.strftime('%H'))
-print(hour)
+
+# print(hour)
+
 if hour<=6 or hour<12:
     print("Good morning",name)
 elif hour<=12 or hour<18:
